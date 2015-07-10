e8c497090c6e09bfc2e3af46c938f7e9a055412b
bbouley <b.bouley@hotmail.co.uk>
2015-07-10 16:03:33 -0600

    loop 6 done

--- loops.js
+++ loops.js
@@ -1,11 +1,18 @@
-for (var i = 0; i <= 1000; i +=100);
+for (var i = 0; i <= 1000; i +=100)
 console.log(i);
 
-for (var i = 1; i <= 128; i *= 2 );
+for (var i = 1; i <= 128; i *= 2 )
 console.log(i);
 
-for (var i = 0; i <= 10; i += 2);
+for (var i = 0; i <= 10; i += 2)
 console.log(i);
 
-for (var i = 0; i <= 16; i ++);
+for (var i = 3; i < 16; i +=3)
 console.log(i);
+
+for (var i = 9; i >= 0; i -= 1)
+console.log(i);
+
+for (var i = 0; i < 5; i ++)
+var triple = i * 3
+console.log(triple);
