35bb57220d68297dd5a37c692283a5d49b2a23c8
bbouley <b.bouley@hotmail.co.uk>
2015-07-10 15:41:15 -0600

    loop 4

--- loops.js
+++ loops.js
@@ -1,8 +1,11 @@
-for (var i = 0; i <= 1000; i +=100)
-console.log(i)
+for (var i = 0; i <= 1000; i +=100);
+console.log(i);
 
-for (var i = 1; i <= 128; i *= 2 )
-console.log(i)
+for (var i = 1; i <= 128; i *= 2 );
+console.log(i);
 
-for (var i = 0; i <= 10; i += 2)
-console.log(i)
+for (var i = 0; i <= 10; i += 2);
+console.log(i);
+
+for (var i = 0; i <= 16; i ++);
+console.log(i);
